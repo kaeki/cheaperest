5912a0e9a247422d144efefbdb943ea9d2aeffbb
kaeki <attesalo@msn.com>
2016-04-29 12:58:09 +0300

    api test 2

--- server.js
+++ server.js
@@ -149,7 +149,8 @@ var SampleApp = function() {
             res.send(favourite);
         };
         self.routes['/bars'] = function (req, res) {
-            var connection_string = getConf();
+            var connection_string = self.getConf();
+            var MongoClient = require('mongodb').MongoClient;  
             // the client db connection scope is wrapped in a callback:
             MongoClient.connect('mongodb://' + connection_string, function (err, db) {
                 if (err) throw err;
