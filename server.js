df8d6dfc00bff622e49b6baed861225032197ab9
kaeki <attesalo@msn.com>
2016-04-29 13:21:41 +0300

    api test 3

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var express = require('express');
 var fs      = require('fs');
 var mongodb = require('mongodb');
 
+
+app.use(function(req, res next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 /**
  *  Define the sample application.
  */
