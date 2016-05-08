615259ee962513d5c876d9a707eb5243f0604988
kaeki <attesalo@msn.com>
2016-05-08 22:18:47 +0300

    added '' '' to id and array name in update function

--- server.js
+++ server.js
@@ -105,8 +105,8 @@ var apiApp = function () {
         MongoClient.connect('mongodb://' + connection_string, function (err, db) {
             if (err) throw err;
             db.collection("bars").updateOne(
-                { _id: id },     // query
-                { $push: {rating: rate} },// update statement
+                { _"id": id },     // query
+                { $push: {"rating": rate} },// update statement
                 function(err,doc) {
                     console.log(doc);
             });
